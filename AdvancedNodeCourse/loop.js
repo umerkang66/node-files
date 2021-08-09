fa05fb8924c420bf4c789385dbd4010910f764d6
umerkang66 <umerkang66@gmail.com>
2021-08-09 22:13:18 +0500

    completed the first section of advanced node js course, internal working of node

--- AdvancedNodeCourse/loop.js
+++ AdvancedNodeCourse/loop.js
@@ -19,6 +19,15 @@ function shouldContinue() {
 }
 
 // entire body executes in 1 tick
-while (shouldContinue()) {}
+while (shouldContinue()) {
+  // 1) Node look at pendingTimers and sees if there are any functions that are ready to be called, then when the timers expires node calls the relevant callbacks, (setTimeOut, and setInterval)
+  // 2) Node looks at pendingOSTasks and pendingOperations and calls relevant callbacks
+  // 3) Pauses execution temporarily. Continue when ...
+  // - when a new pendingOSTasks is done
+  // - when a new pendingOperation is done
+  // - when a timer is about to complete
+  // 4) Look at pending timers (Call any setImmediate)
+  // 5) Handle any 'close' events
+}
 
 // EXIT BACK TO TERMINAL
