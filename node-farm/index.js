58cd508a6db7ba0e0d7ba0e26f085b940b7d6d35
umerkang66 <umerkang66@gmail.com>
2021-08-17 16:19:28 +0500

    finish commit

--- node-farm/index.js
+++ node-farm/index.js
@@ -4,34 +4,6 @@ const fs = require('fs');
 
 const replaceTemplate = require('./modules/replaceTemplate');
 
-/////////////////////////////////
-// FILES
-
-/*
-// BLOCKING, SYNCHROUNOUS WAY
-const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
-const textOutput = `This is what we know about the avacado: ${textInput}\nCreated on ${new Date().toLocaleString()}`;
-fs.writeFileSync('./txt/output.txt', textOutput);
-
-// NON-BLOCKING, ASYNCHRONOUS WAY
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) throw new Error(err.message);
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    if (err) throw new Error(err.message);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      if (err) throw new Error(err.message);
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, err => {
-        if (err) throw new Error(err.message);
-        console.log('Your file has been written 😃😃😃');
-      });
-    });
-  });
-});
-*/
-
-/////////////////////////////////
-// SERVER
-
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
