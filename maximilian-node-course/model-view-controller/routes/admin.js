e2e2478c0f89d8ac65d7c794abcc51d7394f0bae
Umer Kang <umerkang66@gmail.com>
2022-02-01 16:47:01 +0500

    complete the model view controller secition of maximilian node js course section

---
+++ maximilian-node-course/model-view-controller/routes/admin.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const productControllers = require('../controllers/products');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', productControllers.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productControllers.addProduct);
+
+module.exports = router;
