6abe67d810f292a42c903ce6bcb33f65ce121c98
umerkang66 <umerkang66@gmail.com>
2022-04-09 11:57:02 +0500

    add comment in setup.js file in headless-browser testing

--- stephen-advance-node-course/headless-browser-testing-and-travis-ci/tests/setup.js
+++ stephen-advance-node-course/headless-browser-testing-and-travis-ci/tests/setup.js
@@ -1,6 +1,6 @@
 jest.setTimeout(120000);
 
-// We have to tell jest that it is a global file
+// We have to tell jest that it is a global file, so this will run before every test file, (like blogs.test.js and header.test.js)
 require('../models/User');
 
 const mongoose = require('mongoose');
