91f5581db422a5996feb572653ef8800ac508cd7
umerkang66 <umerkang66@gmail.com>
2022-04-20 08:24:13 +0500

    removed files for nest js

--- nest-js-stephen-grider/one-module/src/messages/dtos/create-message.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString } from 'class-validator';
-
-// This will be applied as the type for req body in messages controller
-export class CreateMessageDto {
-  @IsString()
-  content: string;
-}
