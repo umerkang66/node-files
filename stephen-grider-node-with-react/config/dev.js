cfeacbf08d7c8a7e7829b5db313cdb8fde4e6579
umerkang66 <umerkang66@gmail.com>
2022-11-26 13:03:35 +0500

    change from npm to yarn

--- stephen-grider-node-with-react/config/dev.js
+++ stephen-grider-node-with-react/config/dev.js
@@ -2,7 +2,8 @@ module.exports = {
   googleClientId:
     '8803618626-qr2bqddnrti9c7qsa2eledpranc0gbuu.apps.googleusercontent.com',
   googleClientSecret: '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',
-  mongoUri: 'mongodb://localhost:27017/emaily-dev',
+  mongoUri:
+    'mongodb+srv://admin:PLbxzuPfUsCtDfsz@cluster0.7ddsqam.mongodb.net/emaily-dev?retryWrites=true&w=majority',
   cookieKey: 'super super secure cookie key by umer kang',
   stripePublishableKey:
     'pk_test_51KbfY7FCOMVDjfPOQfOE85A6eGjf96CXeiYSsTqb0R3em6yIY6KzmmecNaZVbS4aNr1ZQnPbLOTrOBz4HZmEd3hb006mcqBFzy',
