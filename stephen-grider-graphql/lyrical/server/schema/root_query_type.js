8ec4c70669fa7c93be75738c413379d7604727a3
umerkang66 <umerkang66@gmail.com>
2023-02-21 13:04:43 +0500

    update graphql proj

--- stephen-grider-graphql/lyrical/server/schema/root_query_type.js
+++ stephen-grider-graphql/lyrical/server/schema/root_query_type.js
@@ -13,23 +13,23 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(SongType),
       resolve() {
         return Song.find({});
-      }
+      },
     },
     song: {
       type: SongType,
       args: { id: { type: new GraphQLNonNull(GraphQLID) } },
       resolve(parentValue, { id }) {
         return Song.findById(id);
-      }
+      },
     },
     lyric: {
       type: LyricType,
       args: { id: { type: new GraphQLNonNull(GraphQLID) } },
       resolve(parnetValue, { id }) {
         return Lyric.findById(id);
-      }
-    }
-  })
+      },
+    },
+  }),
 });
 
 module.exports = RootQuery;
