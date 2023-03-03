1dc414e7fe25fb123de189ac8eb87f7bf4e82a2b
umerkang66 <umerkang66@gmail.com>
2023-03-03 17:18:26 +0500

    remove jobs project

--- jonas-node-course/testing-node-apps/jobs/config/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const connectDatabase = () => {
-  mongoose.connect(process.env.DB_LOCAL_URI).then(con => {
-    console.log(`MongoDB Database connected with host: ${con.connection.host}`);
-  });
-};
-
-export default connectDatabase;
