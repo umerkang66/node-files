f9e8590b12fa3c8b3b2d62ca46a5c875eb20e1b9
umerkang66 <umerkang66@gmail.com>
2023-04-08 17:27:26 +0500

    remove package-lock

---
+++ jonas-node-course/typegoose-prj/src/main.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+async function run<T extends () => Promise<void> | void>(func: T) {
+  const mongoUri = 'mongodb://localhost:27017';
+  await mongoose.connect(mongoUri, {
+    dbName: 'typegoose_prj',
+  });
+  console.log('Connected to DB');
+
+  await func();
+  await mongoose.disconnect();
+}
